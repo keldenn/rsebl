34ee05fa11191b12f1a1c36a4bab7d09505bb7ad
KingaPenjor <kpenjor06@gmail.com>
2025-01-03 15:23:17 +0600

    merge

---
+++ app/listedscripts/page.tsx
@@ -0,0 +1,10 @@
+import CompanyCards from "./components/CompanyCards"
+
+export default async function ListedScriptsPage() {
+ 
+  return (
+    <div>
+  <CompanyCards></CompanyCards>
+    </div>
+  )
+}
