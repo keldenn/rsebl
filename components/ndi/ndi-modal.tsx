ff545bc928310776ad7073c92e9129ea504a78a0
KingaPenjor <kpenjor06@gmail.com>
2025-03-19 15:32:21 +0600

    19/03/25 kinga

--- components/ndi/ndi-modal.tsx
+++ components/ndi/ndi-modal.tsx
@@ -8,7 +8,7 @@ import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from 
 import QRCode from 'qrcode';
 const API_BASE_URL =
   process.env.NODE_ENV === "production"
-    ? "https://new-rsebl.vercel.app"
+    ? "http://newrsebl.org.bt:8080/"
     : ""; // Empty means relative path in development
     
 const BhutanNDIButton = ({ text, onClick }) => {
